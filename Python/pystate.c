02443ca770940d13a7215d99e27b20e1de06d36b
Sam Gross <colesbury@gmail.com>
2019-11-01 15:01:45 -0400

    Add mutexes and one-time notifications
    This adds a recursive lock that will be used for locking I/O streams.
    The lock performs a direct handoff if the waiting thread has been paused
    for at least 1 ms. Otherwise, the lock supports barging.
    
    The design is inspired by WTF locks (WebKit) and locking in Go.
    See https://webkit.org/blog/6161/locking-in-webkit/
    
    The "parking lot" implementation will likely need to be improved before
    release. Currently, it uses a fixed-size hashtable (251 taken from Go)
    and the same linked-list for waiters and collisions. Note that Go uses
    a treap (random binary serach tree) for collisions, while WebKit resizes
    the hashtable to ensure 3x as many buckets as threads.

--- Python/pystate.c
+++ Python/pystate.c
@@ -11,6 +11,8 @@
 #include "pycore_sysmodule.h"
 #include "pyatomic.h"
 
+#include "parking_lot.h"
+
 /* --------------------------------------------------------------------------
 CAUTION
 
@@ -667,6 +669,7 @@ void
 _PyThreadState_Init(PyThreadState *tstate)
 {
     tstate->fast_thread_id = _Py_ThreadId();
+    _PyParkingLot_InitThread();
     _PyGILState_NoteThreadState(&tstate->interp->runtime->gilstate, tstate);
 }
 
@@ -888,6 +891,7 @@ tstate_delete_common(PyThreadState *tstate,
 
     if (is_current) {
         _PyThreadState_SET(NULL);
+        _PyParkingLot_DeinitThread();
     }
 }
 
