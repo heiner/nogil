e38bb64cd5a8e6a45bfcac6c12edf07e09de658b
Sam Gross <colesbury@gmail.com>
2020-02-12 16:23:54 -0500

    mimalloc: changes to support separate heaps
    These are changes to support separate heaps for Python objects,
    Python objects with GC header, and non Python objects.

--- Python/pystate.c
+++ Python/pystate.c
@@ -12,6 +12,7 @@
 #include "pycore_refcnt.h"
 
 #include "parking_lot.h"
+#include "mimalloc.h"
 
 /* --------------------------------------------------------------------------
 CAUTION
@@ -34,6 +35,10 @@ to avoid the expense of doing their own locking).
 
 #ifdef __cplusplus
 extern "C" {
+#endif
+
+#if PY_NUM_HEAPS != MI_NUM_HEAPS
+#error "PY_NUM_HEAPS does not match MI_NUM_HEAPS"
 #endif
 
 #define _PyRuntimeGILState_GetThreadState(gilstate) _PyThreadState_GET()
@@ -690,6 +695,9 @@ void
 _PyThreadState_Init(PyThreadState *tstate)
 {
     tstate->fast_thread_id = _Py_ThreadId();
+    for (int tag = 0; tag < Py_NUM_HEAPS; tag++) {
+        tstate->heaps[tag] = mi_heap_get_tag(tag);
+    }
     _PyParkingLot_InitThread();
     _Py_queue_create(tstate);
     _PyGILState_NoteThreadState(&tstate->interp->runtime->gilstate, tstate);
@@ -893,6 +901,11 @@ tstate_delete_common(PyThreadState *tstate,
     if (interp == NULL) {
         Py_FatalError("NULL interpreter");
     }
+
+    for (int tag = 0; tag < Py_NUM_HEAPS; tag++) {
+        tstate->heaps[tag] = NULL;
+    }
+
     _PyRuntimeState *runtime = interp->runtime;
 
     HEAD_LOCK(runtime);
